6b20cf954109411b2b334636e7918206a2de4796
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-06-03 18:55:18 +0200

    added examples of inheritance, static, public, private, private static, and public static

---
+++ example codes/oop/objectsAndClasses.ts
@@ -0,0 +1,55 @@
+
+interface Dog{
+  name: string,
+  age: number,
+  breed: string,
+  owner: string
+}
+
+class Animal {
+  private static tacomonicalClassification: string = 'animals'
+  private kind: string;
+  public name: string;
+  public age: number;
+  constructor(name: string, age: number, kind: string){
+    this.kind = kind;
+    this.name = name;
+    this.age = age;
+  }
+ public static createDog(): Dog{
+    return {
+      name: 'Spot',
+      age: 12,
+      breed: 'mixed',
+      owner: 'Brownbus'
+    }
+  }
+
+static getClassification(){
+   return Animal.tacomonicalClassification
+}
+
+   getKind(){
+    return this.kind
+  }
+}
+
+
+class Monkey extends Animal {
+  constructor(name, age, kind='Monkey'){
+    super(name, age, kind);
+  }
+}
+
+let myAnimal: Animal = new Animal('Horsey', 21, 'Zebra')
+let myDog: Dog = Animal.createDog()
+let myMonkey: Monkey = new Monkey('Luffy', 18)
+
+
+console.log(myDog)
+console.log(myAnimal)
+console.log(myAnimal.getKind())
+console.log(Animal.getClassification())
+console.log(myMonkey)
+console.log(myMonkey.getKind())
+
